e3b61fb9e6be786d67ecde85106430538f1e2a78
Jed Barlow <jbarlow@lavabit.com>
2013-01-12 13:18:36 -0700

    initial commit

---
+++ src/ca/biol398/seqpartitionerplugin/SeqPartitionerPlugin.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2012 Jed Barlow <jbarlow@lavabit.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package ca.biol398.seqpartitioner;
+
+import com.biomatters.geneious.publicapi.plugin.GeneiousPlugin;
+
+public class SeqPartitioner extends GeneiousPlugin {
+    @Override
+    public String getName() {
+        return "Sequence Partitioner Plugin";
+    }
+
+    public String getDescription() {
+        return "Creates partition tables of allele sequence multisets";
+    }
+    public String getHelp() {
+        return "";
+    }
+    public String getAuthors() {
+        return "Jed Barlow <jbarlow@lavabit.com>";
+    }
+    public String getVersion() {
+        return "0.1";
+    }
+    public String getMinimumApiVersion() {
+        return "";  // TODO: figure out what to put here
+    }
+    public int getMaximumApiVersion() {
+        return 6;  // TODO: figure out what to put here
+    }
+}
